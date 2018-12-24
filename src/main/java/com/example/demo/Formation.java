f03893cad2888b4a9b25b9f1c51e94dfd0a627c2
madwols33 <43410677+madwols33@users.noreply.github.com>
2018-12-24 23:41:39 +0100

    Update Formation.java

--- src/main/java/com/example/demo/Formation.java
+++ src/main/java/com/example/demo/Formation.java
@@ -7,6 +7,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlTransient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,11 +21,14 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Formation {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String nom;
 	private int duree;
 	@OneToMany(mappedBy="formation")
+	@JsonIgnore
+	@XmlTransient
 	private Collection<Etudiant> etudiants;
 }
